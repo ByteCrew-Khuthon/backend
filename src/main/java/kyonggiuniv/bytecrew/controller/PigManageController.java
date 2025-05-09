6359d9b9542c9344085afee4ec1654c158b903ec
L33gn21 <l33gn21@gmail.com>
2025-05-09 20:00:52 +0900

    pig barn 환경(온습도 등) 기능

--- src/main/java/kyonggiuniv/bytecrew/controller/PigManageController.java
+++ src/main/java/kyonggiuniv/bytecrew/controller/PigManageController.java
@@ -1,10 +1,14 @@
 package kyonggiuniv.bytecrew.controller;
 
+import kyonggiuniv.bytecrew.entity.BarnEnvironment;
 import kyonggiuniv.bytecrew.service.PigManageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController("/api/manage")
 public class PigManageController {
@@ -15,11 +19,15 @@ public class PigManageController {
         this.pigManageService = pigManageService;
     }
 
-    @PostMapping("/temp")
-    public ResponseEntity<Void> getBarnEnvironment(BarnEnvironment environment){
-        pigManageService.saveBarnEnvironment();
+    @GetMapping("/environment")
+    public ResponseEntity<List<BarnEnvironment>> getBarnEnvironment(){
+        return ResponseEntity.ok(pigManageService.getBarnEnvironments());
+    }
+
+    @PostMapping("/environment")
+    public ResponseEntity<Void> putBarnEnvironment(BarnEnvironment environment){
+        pigManageService.saveBarnEnvironment(environment);
         return ResponseEntity.ok(null);
     }
 
-    public record BarnEnvironment(Double temp, Double humidity){}
 }
