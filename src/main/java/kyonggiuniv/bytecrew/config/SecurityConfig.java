7d70654f281d2f731f4c4f99f42379c18941a69e
2heunxun <seapeon@naver.com>
2025-05-10 00:53:47 +0900

    Test code Deleting

---
+++ src/main/java/kyonggiuniv/bytecrew/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package kyonggiuniv.bytecrew.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+public class SecurityConfig {
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://34.47.66.6:8080","http://34.47.66.6:5173", "http://localhost:5173")); // 허용할 도메인
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // 허용할 HTTP 메서드
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // 허용할 헤더
+        configuration.addExposedHeader("Authorization"); //프론트엔드 요청사항
+        configuration.setAllowCredentials(true); // 쿠키 사용 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 경로에 대해 CORS 설정 적용
+        return source;
+    }
+}
