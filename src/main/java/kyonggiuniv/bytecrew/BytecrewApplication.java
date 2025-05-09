656470907c566409202dc1f91476310f4368cfe0
2heunxun <seapeon@naver.com>
2025-05-09 18:28:43 +0900

    first

---
+++ src/main/java/kyonggiuniv/bytecrew/BytecrewApplication.java
@@ -0,0 +1,13 @@
+package kyonggiuniv.bytecrew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BytecrewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BytecrewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kyonggiuniv/bytecrew/BytecrewApplicationTests.java
@@ -0,0 +1,13 @@
+package kyonggiuniv.bytecrew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BytecrewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
